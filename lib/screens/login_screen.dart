e6176f3a229e41fbd8e4c293a93bbb581f4c7413
kmahsld0602 <76734582+kmahsld0602@users.noreply.github.com>
2024-12-19 20:23:53 +0900

    fix: remove leading button in login screen

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -120,10 +120,7 @@ class _LoginScreenState extends State<LoginScreen>
       appBar: AppBar(
         backgroundColor: Colors.white,
         elevation: 0,
-        leading: IconButton(
-          icon: const Icon(Icons.arrow_back_ios, color: Colors.black),
-          onPressed: () => Navigator.pop(context),
-        ),
+        
         centerTitle: true,
         title: const Text(
           '여정준비',
